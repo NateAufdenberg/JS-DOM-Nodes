e9bf0de681ff9b2323a29c4f17ff346cea2b1da8
Nate Aufdenberg <nlaufdenberg@gmail.com>
2024-09-19 14:03:58 -0500

    All Exercises Completed

--- script.js
+++ script.js
@@ -18,3 +18,57 @@ function showAlert() {
 }
 
 click.addEventListener("click", showAlert);
+
+console.groupEnd();
+
+console.group('Exercise 5');
+
+let input = document.getElementById('textInput');
+let display = document.getElementById('textDisplay');
+
+input.addEventListener('keydown', (event) => {
+   console.log(`${event.key}`)
+});
+
+input.addEventListener('input', () => {
+   display.textContent = input.value;
+});
+/*
+console.groupEnd();
+
+console.group('Exercise 6');
+
+let items = [
+   {name: 'Item 1'},
+   {name: 'Item 2'},
+   {name: 'Item 3'},
+   {name: 'Item 4'}
+];
+
+let list = document.getElementById('itemList');
+
+
+
+items.forEach(item => {
+   const li = document.createElement('li');
+   li.textContent = item;
+   list.appendChild(li);
+});*/
+
+let items = [
+   {name: 'Item 1'},
+   {name: 'Item 2'},
+   {name: 'Item 3'},
+   {name: 'Item 4'}
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+   let ul = document.getElementById('itemList');
+
+   items.forEach(item => {
+      let li = document.createElement('li');
+
+      li.textContent = item.name;
+      ul.appendChild(li);
+   });
+});
